e9a7a80c1f7813b6db21c09fc19c5ed545167ca0
willcurry <watercrafthd@gmail.com>
2017-01-10 15:21:15 +0000

    Add more passing tests

--- GildedRoseTest/GildedRoseTests.cs
+++ GildedRoseTest/GildedRoseTests.cs
@@ -41,12 +41,39 @@ namespace GildedRoseTests
         }
 
         [Test]
-        public void BackstagePassesThatHaveSellInLessThan11AndLessThan50QualityIncrease()
+        public void BackstagePassesThatHaveSellInLessThan11AndHigherThan50QualityDoNotChange()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 51 } };
+            GildedRose GildedRose = new GildedRose(Items);
+            GildedRose.UpdateQuality();
+            Assert.AreEqual(51, Items[0].Quality);
+        }
+
+        [Test]
+        public void BackstagePassesThatHaveSellInLessThan11And45QualityIncreaseTwice()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 45 } };
+            GildedRose GildedRose = new GildedRose(Items);
+            GildedRose.UpdateQuality();
+            Assert.AreEqual(47, Items[0].Quality);
+        }
+
+        [Test]
+        public void BackstagePassesThatHaveSellInLessThan11And49QualityIncreaseOnce()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 } };
             GildedRose GildedRose = new GildedRose(Items);
             GildedRose.UpdateQuality();
             Assert.AreEqual(50, Items[0].Quality);
+        }
+
+        [Test]
+        public void ItemSellInDecreasesByOneWhenNotRecognised()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Item", SellIn = 10, Quality = 0 } };
+            GildedRose GildedRose = new GildedRose(Items);
+            GildedRose.UpdateQuality();
+            Assert.AreEqual(9, Items[0].SellIn);
         }
     }
 }
