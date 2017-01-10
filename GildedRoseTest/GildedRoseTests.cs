52741e3c71646791344b1aac753da57977ff1fe6
willcurry <watercrafthd@gmail.com>
2017-01-10 14:32:09 +0000

    Initial code

---
+++ GildedRoseTest/GildedRoseTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using GildedRoseCS;
+using System.Text;
+using System;
+using System.IO;
+
+namespace GildedRoseTests
+{
+    [TestFixture]
+    public class GildedRoseTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+
+        [Test]
+        public void ThirtyDays()
+        {
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            String output = fakeoutput.ToString();
+            //Approvals.Verify(output); ?
+        }
+    }
+}
