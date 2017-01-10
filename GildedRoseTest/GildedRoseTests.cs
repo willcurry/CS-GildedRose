6cc7a581187fb20225b2bffe0108fba9835da8cc
willcurry <watercrafthd@gmail.com>
2017-01-10 15:05:51 +0000

    Add two passing tests

--- GildedRoseTest/GildedRoseTests.cs
+++ GildedRoseTest/GildedRoseTests.cs
@@ -16,7 +16,7 @@ namespace GildedRoseTests
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
         }
 
         [Test]
@@ -29,6 +29,24 @@ namespace GildedRoseTests
             Program.Main(new string[] { });
             String output = fakeoutput.ToString();
             //Approvals.Verify(output); ?
+        }
+
+        [Test]
+        public void ItemQualityDecreasesOneWhenNotRecognised()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "item", SellIn = 10, Quality = 10 } };
+            GildedRose GildedRose = new GildedRose(Items);
+            GildedRose.UpdateQuality();
+            Assert.AreEqual(9, Items[0].Quality);
+        }
+
+        [Test]
+        public void BackstagePassesThatHaveSellInLessThan11AndLessThan50QualityIncrease()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 } };
+            GildedRose GildedRose = new GildedRose(Items);
+            GildedRose.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
         }
     }
 }
